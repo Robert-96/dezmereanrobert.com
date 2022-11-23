2d8a63e5b00683bfeba2f3a204d6a7ea32ff8622
Robert-96 <dezmereanrobert@gmail.com>
2022-11-23 23:56:58 +0200

    Update dark.js

--- themes/loaf/static/js/dark.js
+++ themes/loaf/static/js/dark.js
@@ -1,4 +1,4 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC (Flash of Unstyled Content)
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC (Flash of Unstyled Content).
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
 } else {
