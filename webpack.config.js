d3b138ef10ab161147c3d34636c85d12dd2548f8
Robert-96 <dezmereanrobert@gmail.com>
2022-11-29 01:07:02 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './themes/loaf/static/js/index.js',
+        dark: './themes/loaf/static/js/dark.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'themes', 'loaf', 'assets', 'js')
+    }
+};
