1e39a483ba84dd4f8e9714bfff443d4ae8950c96
Robert-96 <dezmereanrobert@gmail.com>
2022-11-30 03:00:58 +0200

    Update style for external links

--- themes/loaf/static/js/index.js
+++ themes/loaf/static/js/index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+function updateExternalLinks() {
+  const anchors = document.querySelectorAll('a');
+
+  anchors.forEach((anchor) => {
+    const regex = new RegExp('/' + window.location.host + '/');
+
+    if(!regex.test(anchor.href)) {
+      anchor.setAttribute('target', '_blank');
+    }
+  });
+};
+
+window.addEventListener("load", function() {
+  updateExternalLinks();
+});
