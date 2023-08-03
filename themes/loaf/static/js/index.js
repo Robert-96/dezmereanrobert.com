991e87d1cd6c93700e5a60a5305ac6a0d32ba847
Robert-96 <dezmereanrobert@gmail.com>
2023-08-04 00:24:32 +0300

    Fix lint issues

--- themes/loaf/static/js/index.js
+++ themes/loaf/static/js/index.js
@@ -1,18 +1,18 @@
 'use strict'
 
-function updateExternalLinks() {
-  const anchors = document.querySelectorAll('a');
+function updateExternalLinks () {
+  const anchors = document.querySelectorAll('a')
 
   anchors.forEach((anchor) => {
-    const regex = new RegExp('/' + window.location.host + '/');
+    const regex = new RegExp('/' + window.location.host + '/')
 
     if (!regex.test(anchor.href)) {
-      anchor.setAttribute('target', '_blank');
-      anchor.setAttribute('rel', 'noopener');
+      anchor.setAttribute('target', '_blank')
+      anchor.setAttribute('rel', 'noopener')
     }
-  });
-};
+  })
+}
 
-window.addEventListener('load', function() {
-  updateExternalLinks();
-});
+window.addEventListener('load', function () {
+  updateExternalLinks()
+})
