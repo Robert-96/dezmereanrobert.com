0a2846ba1a2bc9443a89d3192ab0f3f75fd45680
Robert-96 <dezmereanrobert@gmail.com>
2023-08-11 03:09:55 +0300

    Use anchor-js

--- themes/loaf/static/js/index.js
+++ themes/loaf/static/js/index.js
@@ -1,18 +1,29 @@
 'use strict'
 
-function updateExternalLinks () {
-  const anchors = document.querySelectorAll('a')
+import AnchorJS from 'anchor-js';
+
+const anchors = new AnchorJS();
+anchors.options.placement = 'right';
+anchors.options.icon = '#';
+anchors.options.class = 'no-underline';
+
+function updateExternalLinks() {
+  const anchors = document.querySelectorAll('a');
 
   anchors.forEach((anchor) => {
-    const regex = new RegExp('/' + window.location.host + '/')
+    const regex = new RegExp('/' + window.location.host + '/');
 
     if (!regex.test(anchor.href)) {
-      anchor.setAttribute('target', '_blank')
-      anchor.setAttribute('rel', 'noopener')
+      anchor.setAttribute('target', '_blank');
+      anchor.setAttribute('rel', 'noopener');
     }
-  })
+  });
 }
 
-window.addEventListener('load', function () {
+document.addEventListener('DOMContentLoaded', function() {
   updateExternalLinks()
-})
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  anchors.add('.prose h2, .prose h3, .prose h4, .prose h5, .prose h6, .prose h7, .prose h8');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
-module.exports = {
+let config = {
+  target: 'web',
   entry: {
     app: './themes/loaf/static/js/index.js',
     dark: './themes/loaf/static/js/dark.js'
@@ -9,4 +10,12 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'themes', 'loaf', 'assets', 'js')
   }
+}
+
+module.exports = function(env, argv) {
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+  }
+
+  return config;
 }
